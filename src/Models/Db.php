f5d82f468ec0d77dfed3ef9f20bcca932c5d815a
Rémi Michel <remi.michel38@gmail.com>
2022-05-08 15:53:03 +0200

    feat(): DB env

--- src/Models/Db.php
+++ src/Models/Db.php
@@ -7,7 +7,16 @@ class Db {
     protected $db;
 
     public function __construct(){
-        $this->db = new \PDO('mysql:dbname=pixy;host=127.0.0.1', 'root', '');
+        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+        $server = $url["host"];
+        $username = $url["user"];
+        $password = $url["pass"];
+        $db = substr($url["path"], 1);
+
+        var_dump($server, $username, $password, $db);
+
+        $this->db = new \PDO('mysql:dbname=pixy;host=127.0.0.1:8889', 'root', 'root');
     }
 
 
