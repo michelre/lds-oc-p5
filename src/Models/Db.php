7fdea832837664b1f6e677235456f7d834120050
Rémi Michel <remi.michel38@gmail.com>
2022-05-14 23:20:50 +0200

    fix(): db name

--- src/Models/Db.php
+++ src/Models/Db.php
@@ -11,8 +11,8 @@ class Db {
         $server = $url["host"] ?? '127.0.0.1:8889';
         $username = $url["user"] ?? 'root';
         $password = $url["pass"] ?? 'root';
-        //$db = substr($url["path"], 1) ?? 'pixy';
-        $db = 'pixy';
+        $db = substr($url["path"], 1) ?? 'pixy';
+        //$db = 'pixy';
         $this->db = new \PDO("mysql:dbname={$db};host={$server}", $username, $password);
     }
 
