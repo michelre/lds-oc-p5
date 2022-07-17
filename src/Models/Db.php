23cc1da87fc38e5ec3f99cfa0d4c3ad50c8e0b0f
Rémi Michel <remi.michel38@gmail.com>
2022-07-17 16:11:24 +0200

    feat(): add exception on DB connexion

--- src/Models/Db.php
+++ src/Models/Db.php
@@ -8,12 +8,16 @@ class Db {
 
     public function __construct(){
         $url = parse_url(getenv("DATABASE_URL"));
-        $server = $url["host"] ?? '127.0.0.1';
+        $server = $url["host"] ?? '127.0.0.1:8889';
         $username = $url["user"] ?? 'root';
         $password = $url["pass"] ?? 'root';
-        $db = substr($url["path"], 1) ?? 'pixy';
+        $db = substr($url["path"], 1) ? substr($url["path"], 1) : 'pixy';
         //$db = 'pixy';
-        $this->db = new \PDO("mysql:dbname={$db};host={$server}", $username, $password);
+        try {
+            $this->db = new \PDO("mysql:dbname={$db};host={$server}", $username, $password);
+        } catch(\PDOException $e){
+            var_dump("Erreur de connexion à la base de données");
+        }
     }
 
 
